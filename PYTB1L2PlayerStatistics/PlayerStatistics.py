db82d5ee9bc98d59ea9a1325d4ae1eec5b95be25
DaanSchuitemaker <31993@ma-web.nl>
2020-10-23 12:24:24 +0200

    Player statistics

---
+++ PYTB1L2PlayerStatistics/PlayerStatistics.py
@@ -0,0 +1,28 @@
+
+naam = "Daan Schuitemaker"
+level = "200"
+car = "Lamborhini Centenario LP 770-4"
+bhp = "759"
+weight = "1600"
+torque = "689N·m"
+drivetrain = "AWD"
+speed = "8.6"
+handling = "9.4"
+acceleration = "10"
+launch = "10"
+braking = "9.8"
+PI = "S2 918"
+
+print('name: ' + naam)
+print('level: ' + level)
+print('car: ' + car)
+print('bhp: ' + bhp)
+print('weight: ' + weight + 'kg')
+print('torque: ' + torque)
+print('drivetrain: ' + drivetrain)
+print('speed: ' + speed)
+print('handling: ' + handling)
+print('acceleration: ' + acceleration)
+print('launch: ' + launch)
+print('braking: ' + braking)
+print('PI: ' + PI)
