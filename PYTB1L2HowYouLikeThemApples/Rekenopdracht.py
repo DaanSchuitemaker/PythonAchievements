48bf5a0e631f67194d0ff2140e0c298835bf69b5
DaanSchuitemaker <31993@ma-web.nl>
2020-10-23 12:23:28 +0200

    How you like them apples

---
+++ PYTB1L2HowYouLikeThemApples/Rekenopdracht.py
@@ -0,0 +1,26 @@
+trees = 333
+print(trees)
+shadedTrees = 333 / 2 / 3
+print(shadedTrees)
+sunnyTrees = 333 / 3
+print(sunnyTrees)
+
+shadeOutputModifier  = 0.8
+print(shadeOutputModifier)
+
+sunnyTreeOutput = 146
+print(sunnyTreeOutput)
+shadedTreeOutput = 146 * shadeOutputModifier
+print(shadedTreeOutput)
+
+sunnyOutput = sunnyTrees * sunnyTreeOutput
+print(sunnyOutput)
+shadedOutput = shadedTrees * shadedTreeOutput
+print(shadedOutput)
+totalOutput = sunnyOutput + shadedOutput
+print(totalOutput)
+
+owners = 4
+print(owners)
+
+
