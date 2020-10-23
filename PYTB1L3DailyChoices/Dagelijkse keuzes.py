c20f70a9be5fa5e071ec207449d6cf7cadd092f5
DaanSchuitemaker <31993@ma-web.nl>
2020-10-23 12:25:07 +0200

    Daily choices

---
+++ PYTB1L3DailyChoices/Dagelijkse keuzes.py
@@ -0,0 +1,53 @@
+print("Je alarm gaat af, ga je UIT BED of BLIJF LIGGEN?")
+keuze = input()
+if keuze == "UIT BED":
+    print("Goed bezig!")
+elif keuze == "BLIJF LIGGEN":
+    print("Slaapkop...")
+else:
+    print(keuze, "was niet een optie")
+
+print()
+
+print("Voor ontbijt eet je YOGHURT of een BOTERHAM?")
+keuze = input()
+if keuze == "YOGHURT":
+    print("mmmm lekker")
+elif keuze == "BOTERHAM":
+    print("mmmm lekker")
+else:
+    print(keuze, "was niet een optie")
+
+print()
+
+print("Ga je met de TREIN of FIETS naar school?")
+keuze = input()
+if keuze == "TREIN":
+    print("Als de NS maar op tijd is")
+elif keuze == "FIETS":
+    print("Dat ga je niet redden")
+else:
+    print(keuze, "was niet een optie")
+
+print()
+
+print("Ga je je best doen op school JA of NEE?")
+keuze = input()
+if keuze == "JA":
+    print("Mooi zo")
+elif keuze == "NEE":
+    print("DOe toch maar wel")
+else:
+    print(keuze, "was niet een optie")
+
+print()
+
+print("Als je thuis komt ga je HUISWERK doen of ga je GAMEN?")
+keuze = input()
+if keuze == "HUISWERK":
+    print("Goed bezig")
+elif keuze == "GAMEN":
+    print("Je mag best even relaxen")
+else:
+    print(keuze, "was niet een optie")
+    
